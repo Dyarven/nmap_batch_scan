ea6556e08d10d8a0cc122c942860ed3f691a3736
Dyarven <81330265+Dyarven@users.noreply.github.com>
2025-04-08 19:47:21 +0200

    Update batch_scanner.sh

--- batch_scanner.sh
+++ batch_scanner.sh
@@ -2,21 +2,21 @@
 
 filename="$1"
 port="$2"
-threads=8  # system threads
-output_file="result.txt"
+threads=8
+outfile="result.txt"
 temp_dir=$(mktemp -d)
 
-echo "Running nmap, this might take a while..."
+total_ips=$(wc -l < "$filename")
 
-# Use xargs to handle batching and parallel execution
-xargs -a "$filename" -L 100 -P "$threads" -I {} sh -c '
+echo "Running nmap scan for port $port across $total_ips IPs..."
+
+# Progress bar
+pv -l -s "$total_ips" "$filename" | xargs -L 100 -P "$threads" -I {} sh -c '
     ips=$(echo {} | tr " " ",")
     result=$(nmap --host-timeout 30s -T4 -n -p "$0" -oG - "$ips" 2>/dev/null)
     echo "$result" | awk '\''/open/ {gsub(/[()]/,"",$2); print $2}'\'' >> "$1"/temp.$$
 ' "$port" "$temp_dir"
 
-# Combine results and clean up
-cat "$temp_dir"/* >> "$output_file"
+cat "$temp_dir"/* >> "$outfile"
 rm -rf "$temp_dir"
-
-echo "finished scanning IP list"
+echo "Scan completed. Results saved to $outfile"
